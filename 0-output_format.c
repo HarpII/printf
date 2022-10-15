8f9962bf6356ea027f8cab0eb94204bc15290104
Harp <walesii19@gmail.com>
2022-10-15 14:33:20 +0000

    renamed output_format file

---
+++ 0-output_format.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: string with indentifiers
+ *
+ * Return: number of characters printed
+ */
+int _print(const char *format, ...);
+{
+	
+}
